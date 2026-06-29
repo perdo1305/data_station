b4e04af9d9755f84d000812082021033728d5055
joao-sintra <sintrajoao14@gmail.com>
2026-06-29 22:42:17 +0100

    mission none = manual

--- LART_Car_Dashboard_v1/src/ui/generate_dbc_api.py
+++ LART_Car_Dashboard_v1/src/ui/generate_dbc_api.py
@@ -269,7 +269,7 @@ def main():
         "        case 4: return \"BRAKE TEST\";",
         "        case 5: return \"INSPECTION\";",
         "        case 6: return \"AUTOCROSS\";",
-        "        default: return \"NONE\";",
+        "        default: return \"MANUAL\";",
         "    }",
         "}"
     ])
