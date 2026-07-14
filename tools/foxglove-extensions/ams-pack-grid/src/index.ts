727613a102df3d7aec0cc033b1ea282a3e086160
joao-sintra <sintrajoao14@gmail.com>
2026-07-15 00:19:52 +0100

    revert: remove AMS pack grid Foxglove extension
    No longer wanted.

--- tools/foxglove-extensions/ams-pack-grid/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { ExtensionContext } from "@foxglove/extension";
-
-import { initAmsPackGrid } from "./AmsPackGrid";
-
-export function activate(extensionContext: ExtensionContext): void {
-  extensionContext.registerPanel({ name: "AMS Pack Grid", initPanel: initAmsPackGrid });
-}
